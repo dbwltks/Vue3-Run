42db28d9108e6a30e7d129809ccf9f64237811e6
Ji San <dbwltks@gmail.com>
2022-03-28 18:04:46 +0900

    fifa4 event(참여내역, 당첨자명단)

--- src/components/VIFR.vue
+++ src/components/VIFR.vue
@@ -1,11 +1,11 @@
 <template>
   <p>참여 횟수 : {{TryCount}}</p>
   <p>선택 : {{PickValue}}</p>
-  <button type="button" @click="Ticket++">참여내역</button>
-  <p>당첨명단 : </p>
+  <button type="button" @click="onJoinList">참여내역</button>
+  <p>당첨명단 : {{PrizeList}}</p>
   <!-- <br/> -->
-  <button type="button" @click="Ticket++">참여권 1개 구매하기</button>
-  <button type="button" @click="Ticket+=3">참여권 3개 구매하기</button>
+  <button type="button" @click="onTicket(1)">참여권 1개<br/>1,390 FC/MC <br/>구매하기</button>
+  <button type="button" @click="onTicket(3)">참여권 3개 <br/>5,070 FC/MC<br/>구매하기</button>
   <br/>
   <button type="button" class="_left_1" @click="onClick('L_1', 1, 2, $event)">니어포스트</button>
   <button type="button" class="_right_1" @click="onClick('R_1', 2, 1, $event)">파포스트</button>
@@ -33,10 +33,10 @@
   <hr/>
   <br/>
   <p>누적 실패 경험치 {{FailExp}}회(7회) -> 보유 키컨 포인트 {{BonusPoint}}P</p>
-  <button @click="onPointItem(30,$event)">30포인트 교환하기</button>
-  <button @click="onPointItem(20,$event)">20포인트 교환하기</button>
-  <button @click="onPointItem(10,$event)">10포인트 교환하기</button>
-  <button @click="onPointItem(2,$event)">2포인트 교환하기</button>
+  <button @click="onPointItem(30,$event)">30포인트 <br/>교환하기</button>
+  <button @click="onPointItem(20,$event)">20포인트 <br/>교환하기</button>
+  <button @click="onPointItem(10,$event)">10포인트 <br/>교환하기</button>
+  <button @click="onPointItem(2,$event)">2포인트 <br/>교환하기</button>
 </template>
 
 <script>
@@ -67,6 +67,8 @@ export default {
     let PointItem = ref(['1/N 스페셜판 SSS,SS 상자', '스페셜판 SSS,SS 상자', '스페셜판 SSS,SS,S 상자', '[고수 상자 기회 10%] 행운의 BP 카드 (1억 ~ 5억 BP)'])
     let JoinItem = ref(['[키컨달인] 행운의 BP 카드 (100만 ~ 5억)'])
     let IndemList = ref([])
+    let JoinList = ref([])
+    let PrizeList = ref([])
     return {
       Ticket,
       FailExp,
@@ -87,6 +89,8 @@ export default {
       PointItem,
       JoinItem,
       IndemList,
+      JoinList,
+      PrizeList,
     }
   },
   methods: {
@@ -159,37 +163,43 @@ export default {
             this.BingoCount += -1
           }
         }
-        if(this.BingoCount == 6 || this.BingoCount == -6) { //SSS, 완벽한 선방!
-          this.ComValue = '[SSS] 빙고'
+        if(this.BingoCount == 6) { //SSS, 완벽한 선방! 
+          this.ComValue = '[SSS] 완벽한 선방! '
           this.IndemList[0] = this.SSSItem[Math.ceil(Math.random()*2)]
-          this.IndemList[1] = this.JoinItem[0]
+          this.PrizeList = '피***인 구단주님이 "'+this.IndemList[0]+'" 아이템에 당첨되었습니다.'
+        } else if(this.BingoCount == -6) { //SSS, 완벽한 실패..
+          this.ComValue = '[SSS] 완벽한 실패.. '
+          this.IndemList[0] = this.SSSItem[Math.ceil(Math.random()*2)]
+          this.PrizeList = '피***인 구단주님이 "'+this.IndemList[0]+'" 아이템에 당첨되었습니다.'
+          // this.IndemList[1] = this.JoinItem[0]
         } else if (this.BingoCount == 4) { //SS, 거의 완벽한 선방! 실패경험치 +1
-          this.ComValue = '[SS] 6번 중 5번 성공!'
+          this.ComValue = '[SS] 6번 중 5번 성공! '
           this.FailExp += 1
           this.IndemList[0] = this.SSItem[Math.ceil(Math.random()*2)]
-          this.IndemList[1] = this.JoinItem[0]
+          // this.IndemList[1] = this.JoinItem[0]
         } else if (this.BingoCount == -4) { //B, 평균적 선방!  실패경험치 +5
-          this.ComValue = '[B] 6번 중 1번 성공!'
+          this.ComValue = '[B] 6번 중 1번 성공! '
           this.FailExp += 5
           this.IndemList[0] = this.BItem[Math.ceil(Math.random()*3)]
-          this.IndemList[1] = this.JoinItem[0]
+          // this.IndemList[1] = this.JoinItem[0]
         } else if (this.BingoCount == 2)  { //S, 성공적 선방! 실패경험치 +2
-          this.ComValue = '[S] 6번 중 4번 성공!'
+          this.ComValue = '[S] 6번 중 4번 성공! '
           this.FailExp += 2
           this.IndemList[0] = this.SItem[Math.ceil(Math.random()*3)]
-          this.IndemList[1] = this.JoinItem[0]
+          // this.IndemList[1] = this.JoinItem[0]
         } else if (this.BingoCount == -2)  { //B, 평균적 선방! 실패경험치 +4
-          this.ComValue = '[B] 6번 중 2번 성공!'
+          this.ComValue = '[B] 6번 중 2번 성공! '
           this.FailExp += 4
           this.IndemList[0] = this.BItem[Math.ceil(Math.random()*3)]
-          this.IndemList[1] = this.JoinItem[0]
+          // this.IndemList[1] = this.JoinItem[0]
         } else if (this.BingoCount == 0)  { //A, 절반의 선방! 실패경험치 +3
-          this.ComValue = '[A] 6번 중 3번 성공!'
+          this.ComValue = '[A] 6번 중 3번 성공! '
           this.FailExp += 3
           this.IndemList[0] = this.AItem[Math.ceil(Math.random()*3)]
-          this.IndemList[1] = this.JoinItem[0]
+          // this.IndemList[1] = this.JoinItem[0]
         }
         alert("서버 : "+this.ResultValue+"\n선택 : "+this.PickValue+"\n비교 : "+this.EqualValue+"\n결과 : "+this.ComValue+"\n보상 : "+this.IndemList)
+        this.JoinList += '스페셜 게임 참여 | '+this.ComValue+'\n'+this.IndemList+'\n'
         this.Ticket += -1
         this.BingoCount = 0
         this.EqualValue = []
@@ -204,19 +214,37 @@ export default {
         this.CheckList = 0
       } 
     },
+    onJoinList: function(evt)  { //구분(일반 게임 참여, 일반 참여권 구매), 게임 결과(선방성공 회\n선방실패 회), 획득 아이템, 키컨 포인트(사용), 수령일시(YYYY.MM.DD HH:mm)
+      alert(this.JoinList)
+    },
+    onTicket: function(value,evt) {
+      if (value == 1) {
+        alert('스페셜 참여권 구매완료')
+        this.Ticket += 1
+        this.JoinList += '스페셜 참여권 구매 | '+this.JoinItem[0]+'\n'
+      } else if (value == 3)  {
+        alert('스페셜 참여권 3개 구매완료')
+        this.Ticket += 3
+        this.JoinList += '스페셜 참여권 3개 구매 | '+this.JoinItem[0]+' 3개 \n'
+      }
+    },
     onPointItem: function(value, evt)  {
-      if (value == 30 && this.BonusPoint >= 30)  {
-        alert(this.PointItem[0]+'교환완료')
-        this.BonusPoint += -30
-      } else if (value == 20 && this.BonusPoint >= 20) {
-        alert(this.PointItem[1]+'교환완료')
-        this.BonusPoint += -20
-      } else if (value == 10 && this.BonusPoint >= 10) {
-        alert(this.PointItem[2]+'교환완료')
-        this.BonusPoint += -10
-      } else if (value == 2 && this.BonusPoint >= 2)  {
-        alert(this.PointItem[3]+'교환완료')
-        this.BonusPoint += -2
+      if (value == 30 && this.BonusPoint >= value)  {
+        alert(this.PointItem[0]+' 교환완료')
+        this.JoinList += '교환 | '+this.PointItem[0]+' '+this.BonusPoint+'P -> '+(this.BonusPoint-value)+'P\n'
+        this.BonusPoint += -value
+      } else if (value == 20 && this.BonusPoint >= value) {
+        alert(this.PointItem[1]+' 교환완료')
+        this.JoinList += '교환 | '+this.PointItem[1]+' '+this.BonusPoint+'P -> '+(this.BonusPoint-value)+'P\n'
+        this.BonusPoint += -value
+      } else if (value == 10 && this.BonusPoint >= value) {
+        alert(this.PointItem[2]+' 교환완료')
+        this.JoinList += '교환 | '+this.PointItem[2]+' '+this.BonusPoint+'P -> '+(this.BonusPoint-value)+'P\n'
+        this.BonusPoint += -value
+      } else if (value == 2 && this.BonusPoint >= value)  {
+        alert(this.PointItem[3]+' 교환완료')
+        this.JoinList += '교환 | '+this.PointItem[3]+' '+this.BonusPoint+'P -> '+(this.BonusPoint-value)+'P\n'
+        this.BonusPoint += -value
       } else {
         alert('포인트가 부족합니다.')
       }
